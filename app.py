4a7d68385b35f815524ff071485ff232d0664511
SHUBHAM BOPCHE <116349421+Shubhambopche29@users.noreply.github.com>
2023-05-21 14:42:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 21 14:16:29 2023
+
+@author: shubham
+"""
 import streamlit as st
 import pickle
 import random
+from PIL import Image
 
 # load the model
 model = pickle.load(open('customer_model.pkl', 'rb'))
 
+pickle_in = open("customer_model.pkl", "rb")
+customer_model = pickle.load(pickle_in)
+
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://p4.wallpaperbetter.com/wallpaper/25/1010/360/nature-black-widow-light-digital-wallpaper-preview.jpg");
+             background-attachment: fixed;
+	     background-position:75%;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+add_bg_from_url()
+
+def welcome():
+    return "Welcome All"
+
+def predict_note_authentication(Education, Income, Kidhome, Teenhome, Recency, MntWines, MntFruits, MntMeatProducts, MntFishProducts, MntSweetProducts, MntGoldProds, NumDealsPurchases, NumWebPurchases, NumCatalogPurchases, NumStorePurchases, NumWebVisitsMonth,
+        AcceptedCmp3, AcceptedCmp4, AcceptedCmp5, AcceptedCmp1, AcceptedCmp2, Complain, Response, Age, Spent, Living_With,
+        Children, Family_Size, Is_Parent, Total_Promos):
+
+    prediction =customer_model.predict([[Education, Income, Kidhome, Teenhome, Recency, MntWines, MntFruits, MntMeatProducts, MntFishProducts, MntSweetProducts, MntGoldProds, NumDealsPurchases, NumWebPurchases, NumCatalogPurchases, NumStorePurchases, NumWebVisitsMonth,
+        AcceptedCmp3, AcceptedCmp4, AcceptedCmp5, AcceptedCmp1, AcceptedCmp2, Complain, Response, Age, Spent, Living_With,
+        Children, Family_Size, Is_Parent, Total_Promos]])
+    return prediction[0]
+
+# noinspection PyUnreachableCode
 def main():
-    # Set a random background template
-    bg_templates = ["primary", "secondary", "info", "success", "warning", "danger", "light", "dark"]
-    bg_template = random.choice(bg_templates)
-    st.markdown(f'<style>.reportview-container .main .block-container{{background-color: {bg_template};}}</style>', unsafe_allow_html=True)
-
-    st.title("Customer Prediction")
+    st.title("Customer Personality Analysis")
     st.write("Enter the customer details below:")
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">Streamlit Customer Personality Analysis ML App </h2>
+    </div>
+    """
 
+    st.markdown(html_temp, unsafe_allow_html=True)
     Education = st.number_input("Education")
     Income = st.number_input("Income")
     Kidhome = st.number_input("Kidhome")
@@ -44,22 +85,56 @@ def main():
     Family_Size = st.number_input("Family_Size")
     Is_Parent = st.number_input("Is_Parent")
     Total_Promos = st.number_input("Total_Promos")
+     
 
-    @st.cache()
-    def predict():
-        customer_data = [[Education, Income, Kidhome, Teenhome, Recency, MntWines, MntFruits, MntMeatProducts, MntFishProducts, MntSweetProducts, MntGoldProds, NumDealsPurchases, NumWebPurchases, NumCatalogPurchases, NumStorePurchases, NumWebVisitsMonth,
-        AcceptedCmp3, AcceptedCmp4, AcceptedCmp5, AcceptedCmp1, AcceptedCmp2, Complain, Response, Age, Spent, Living_With,
-        Children, Family_Size, Is_Parent, Total_Promos]]
-        result = model.predict(customer_data)[0]
-        return result
-
-    # Predict button
+    result = ""
     if st.button("Predict"):
         try:
-            result = predict()
-            st.write("Prediction:", result)
-        except Exception as e:
+            Education = object(Education)
+            Income = float(Income)
+            Kidhome = int(Kidhome)
+            Teenhome =int (Teenhome)
+            Recency =int (Recency)
+            MntWines =int (MntWines)
+            MntFruits =int (MntFruits)
+            MntMeatProducts =int (MntMeatProducts)
+            MntFishProducts =int (MntFishProducts)
+            MntSweetProducts =int (MntSweetProducts)
+            MntGoldProds = int(MntGoldProds)
+            NumDealsPurchases =int (NumDealsPurchases)
+            NumWebPurchases =int(NumWebPurchases)
+            NumCatalogPurchases =int (NumCatalogPurchases)
+            NumStorePurchases =int (NumStorePurchases)
+            NumWebVisitsMonth =int (NumWebVisitsMonth)
+            AcceptedCmp3 =int (AcceptedCmp3)
+            AcceptedCmp4 =int (AcceptedCmp4)
+            AcceptedCmp5 =int (AcceptedCmp5)
+            AcceptedCmp1 = int(AcceptedCmp1)
+            AcceptedCmp2 =int (AcceptedCmp2)
+            Complain =int(Complain)
+            Response =int (Response)
+            Age =int(Age)
+            Spent =int (Spent)
+            Living_With =object  (Living_With)
+            Children =int (Children)
+            Family_Size =int (Family_Size)
+            Is_Parent =int (Is_Parent)
+            Total_Promos =int (Total_Promos)
+
+    
+    
+            Result =  predict_note_authentication(Education, Income, Kidhome, Teenhome, Recency, MntWines, MntFruits, MntMeatProducts, MntFishProducts, MntSweetProducts, MntGoldProds, NumDealsPurchases, NumWebPurchases, NumCatalogPurchases, NumStorePurchases, NumWebVisitsMonth,
+            AcceptedCmp3, AcceptedCmp4, AcceptedCmp5, AcceptedCmp1, AcceptedCmp2, Complain, Response, Age, Spent, Living_With,
+            Children, Family_Size, Is_Parent, Total_Promos)
+        
+            st.success('The output is {}'.format(result))
+        except:
             st.error("An error occurred during prediction. Please check your inputs and try again.")
+
+                                  
+    if st.button("About"):
+          st.text("Built with Streamlit")                                
+                                  
 
 if __name__ == '__main__':
     main()
